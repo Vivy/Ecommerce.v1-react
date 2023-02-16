f56f904247e03d555900664a3e61fb863c6512d4
Vivy <Tomitaviorel@yahoo.com>
2023-02-16 11:02:23 +0200

    feat: added compoenents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './component/navbar';
+import Home from './pages/home/home';
+import Cart from './pages/cart/cart';
+
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
