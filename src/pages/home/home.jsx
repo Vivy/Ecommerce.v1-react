ef38f3bfb841df5b99126b64e77c82b407432fb5
Vivy <Tomitaviorel@yahoo.com>
2023-02-16 11:22:12 +0200

    feat: added media

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className='shop'>
+        <div className='shopTitle'>
+          <h1>Vivy`s Shop</h1>
+        </div>
+        <div className='products'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import iphone from './assets/1.png'
+import laptop from './assets/2.png'
+import canon from './assets/3.png'
+import jacket from './assets/4.png'
+import ledStrip from './assets/5.png'
+import jumper from './assets/6.webp'
+import shirt from './assets/7.webp'
+import cap from './assets/8.webp'
+
+export const products = [
+  {
+    id: 1,
+    productName: 'Iphone',
+    price: 999,
+    productImg: iphone,
+  },
+  {
+    id: 2,
+    productName: 'Laptop',
+    price: 2000,
+    productImg: laptop,
+  },
+  {
+    id: 3,
+    productName: 'Canon Camera',
+    price: 650,
+    productImg: canon,
+  },
+  {
+    id: 4,
+    productName: 'Jacket Dior',
+    price: 110,
+    productImg: jacket,
+  },
+  {
+    id: 5,
+    productName: 'Led Strip',
+    price: 25,
+    productImg: ledStrip,
+  },
+  {
+    id: 6,
+    productName: 'Armani Jumper',
+    price: 45,
+    productImg: jumper,
+  },
+  {
+    id: 7,
+    productName: 'Shirt Golf',
+    price: 30,
+    productImg: shirt,
+  },
+  {
+    id: 8,
+    productName: 'Cap Gigi',
+    price: 50,
+    productImg: cap,
+  },
+]
